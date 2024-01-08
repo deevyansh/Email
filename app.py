489f51bf143de9a45f95e2865eaaa298b76d4d64
Deevyansh Khadria <devkhadria@gmail.com>
2024-01-08 21:07:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+nltk.download('punkt')
+nltk.data.path.append("/path/to/nltk_data")
+from nltk.tokenize import  word_tokenize
+# Now you can use the NLTK library for tokenization
+
+tfidf=pickle.load(open('vectorizer.pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))
+st.title("Email/SMS Spam Classifier")
+input_sms =st.text_input("Enter the Message")
+
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+
+
+def transform_text(text):
+  text=text.lower()
+  text=nltk.word_tokenize(text)
+  y=[]
+  for i in text:
+    if i.isalnum():
+      y.append(i)
+  text=y[:]
+  y.clear()
+  for i in text:
+    if i not in nltk.corpus.stopwords.words('english'):
+      if i not in string.punctuation:
+        y.append(i)
+  text=y[:]
+  y.clear()
+  for i in text:
+    y.append(ps.stem(i))
+  return " ".join(y)
+
+if st.button('Predict'):
+  # 1. preprocess
+  transformed_sms=(input_sms)
+  # 2. vectorize
+  vector_input=tfidf.transform([transformed_sms])
+  #predict
+  result=model.predict(vector_input)[0]
+  #Display
+  if result==1:
+      st.header("Spam")
+  else:
+      st.header("Not Spam")
+
